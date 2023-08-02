114a7c7e35cac55f9e112393dc0babc2036337b7
Lorena Defalco <l.defalco@mjus.gba.gob.ar>
2023-08-02 17:43:15 -0300

    inicial

--- instancia.js
+++ instancia.js
@@ -6,19 +6,18 @@ createApp({
         mensaje:'Hola desde instancia uno',
         numero1: 10,
         numero2: 30,
-        mensaje2: 'Hola desde instancia 2'
+        //mensaje2: 'Hola desde instancia 2'
     }
     
 }
-}).mount('#instanciaUno')
+}).mount('#instanciaUno');
 
 
-// const { createApp2 } = Vue 
+createApp({
+    data() {
+       return {
+        mensaje2: 'Hola desde instancia 2'
+       }
+}
+}).mount('#instanciaDos')
 
-// createApp2({
-//     data() {
-//        return {
-//         mensaje2: 'Hola desde instancia 2'
-//        }
-// }
-// }).mount('#instanciaDos')
